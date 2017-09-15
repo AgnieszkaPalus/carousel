8639f16576ce56ba896c2dd1063505232cdd47f5
Agnieszka Palus <a.palus@gmail.com>
2017-09-15 12:41:25 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-	// Set the interval to be 5 seconds
 	var t = setInterval(function(){
 		$("#carousel ul").animate({marginLeft:-400},1000,function(){
 			$(this).find("li:last").after($(this).find("li:first"));
